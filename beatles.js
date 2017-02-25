98604cfbca7087628748f376be0d6d7f4499df49
Ali Diyab <alidiyab1997@gmail.com>
2017-02-25 13:42:05 +0000

    Done.

---
+++ beatles.js
@@ -0,0 +1,28 @@
+function theBeatlesPlay(musicians,instruments){
+  var result=[];
+  for(var i=0; i< musicians.length;i++)
+  {
+  	result.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return result;
+}
+
+function johnLennonFacts(facts){
+  var i = 0
+  while(i < facts.lenght){
+    facts[i]=facts[i]+"!!!";
+    i=i+1;
+  }
+  return facts
+}
+
+function iLoveTheBeatles(number){
+	var result=[];
+	do{
+		result.push("I love the Beatles!");
+		number++;
+	}
+	while(number < 15);{
+	return result;
+}
+}
